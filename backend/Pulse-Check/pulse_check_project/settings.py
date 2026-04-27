f6ad6ccbeb04bb085a430362def3cad58443d34c
uncletoon <ingabirepatience3@gmail.com>
2026-04-28 01:13:26 +0300

    chore(docker): containerize Django with Celery and Radis

--- backend/Pulse-Check/pulse_check_project/settings.py
+++ backend/Pulse-Check/pulse_check_project/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
 }
 
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
